ba73756d05933f0a106111d00a7a550dccb2bf2c
Zack Weinberg <zack@owlfolio.org>
2025-05-06 14:56:40 -0400

    Small adjustments to presentation content and logic
    - pull the Reveal init script out to a .js file so it works when
      inline scripts are forbidden
    - move all inline styles to the theme, for the same reason
    - add Million Concepts logo to first slide
    - add financial support disclosure to last slide
    - retitle last slide to ‘acknowledgments’
    - summarize CheckSignalsOftenEnough code, add link to full version
    - add link to CPython PR to make PyErr_CheckSignals not need the GIL

---
+++ pycon-2025/rjs/init.js
@@ -0,0 +1,12 @@
+// More info about initialization & config:
+// - https://revealjs.com/initialization/
+// - https://revealjs.com/config/
+Reveal.initialize({
+    hash: true,
+    // Learn about plugins: https://revealjs.com/plugins/
+    plugins: [ RevealNotes ],
+    transition: "fade",
+    center: false,
+    width: 1920,
+    height: 1080,
+});
